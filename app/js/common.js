7e6a9f03432d2e6e6cdeb18843b594017cb6cf67
BakayDev <gamempvs@gmail.com>
2020-10-04 19:31:58 +0600

    added mmenu style less 5

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
-
-	// Custom JS
-
+    $('#my-menu').mmenu({
+        extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: '<img src="img/logo-1.svg" alt="Салон красоты">'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
+    let api = $('#my-menu').data('mmenu');
+    api.bind('opened', function () {
+        $('.hamburger').addClass('is-active');
+    }).bind('closed', function () {
+        $('.hamburger').removeClass('is-active');
+    })
 });
